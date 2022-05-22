fd129da30d285ec3322475eb444343971259835d
didicozaur <moisin.diana@yahoo.com>
2022-05-22 14:28:02 +0200

    added Home

---
+++ src/pages/Home.js
@@ -0,0 +1,14 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <div className="hero">
+        <h1>Find easy...learn today</h1>
+        <img src="../images/education1.jpg" alt="education1" />
+      </div>
+    </>
+  );
+}
+
+export default Home;
