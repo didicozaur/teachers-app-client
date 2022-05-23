a74e9f93c1d578b7700cb070ab11889bc9190674
didicozaur <moisin.diana@yahoo.com>
2022-05-23 07:54:42 +0200

    minor changes

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,7 +5,6 @@ function Home() {
     <>
       <div className="hero">
         <h1>Find easy...learn today</h1>
-        <img src="../images/education1.jpg" alt="education1" />
       </div>
     </>
   );
--- src/pages/SubjectsPage.js
+++ src/pages/SubjectsPage.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import AddSubject from "../components/AddSubject";
 
 function SubjectsPage() {
   const [subjects, setSubjects] = useState([]);
