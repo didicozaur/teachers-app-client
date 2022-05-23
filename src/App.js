3fbc65e834b67ed82877c93d54fb627ff0112fcb
didicozaur <moisin.diana@yahoo.com>
2022-05-23 07:56:33 +0200

    added Routes

--- src/App.js
+++ src/App.js
@@ -1,40 +1,46 @@
-
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './components/Footer';
-import Navbar from './components/NavBar';
-import AdDetails from './pages/AdDetails';
-import AdList from './pages/AdList';
-import 'bootstrap/dist/css/bootstrap.min.css'
-
-
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Navbar from "./components/NavBar";
+import AdDetails from "./pages/AdDetails";
+import AdList from "./pages/AdList";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Home from "./pages/Home";
+import SubjectsPage from "./pages/SubjectsPage";
+import AddSubject from "./pages/AddSubject";
 
 function App() {
-
   const [ads, setAds] = useState([]);
 
   const fetchAds = () => {
-  useEffect(() => {
     axios
       .get(`${process.env.REACT_APP_API_URL}/ads`)
       .then((response) => {
         setAds(response.data);
       })
       .catch((err) => console.log("Error getting ads from DB", err));
-  }, []);}
-
+  };
+  useEffect(() => {
+    fetchAds();
+  }, []);
 
   return (
     <>
       <Navbar />
 
       <Routes>
-        <Route path="/ads" element={ <AdList ads={ads}/> }/>
-        <Route path="/ads/:adId" element={<AdDetails adId={adId} />} updatePage={fetchAds}/>
+        <Route path="/" element={<Home />} />
+        <Route path="/subjects" element={<SubjectsPage />} />
+        <Route path="/subjects/add" element={<AddSubject />} />
+        <Route path="/ads" element={<AdList ads={ads} />} />
+        <Route
+          path="/ads/:adId"
+          element={<AdDetails />}
+          updatePage={fetchAds}
+        />
       </Routes>
-     
 
       <Footer />
     </>
