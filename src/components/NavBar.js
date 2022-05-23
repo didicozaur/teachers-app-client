e4075f4326bcc7c53cc6c7a0b64dfaf4479a01ee
didicozaur <moisin.diana@yahoo.com>
2022-05-23 07:47:31 +0200

    added two NavLinks

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,25 +3,22 @@ import { NavLink } from "react-router-dom";
 import { AuthContext } from "../context/auth.context";
 import logo from "../images/logo.png";
 
-
 function Navbar() {
   const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
 
   return (
     <nav className="Navbar">
-    <img src={logo} alt="logo" width="80px"></img> |
-      <NavLink to="/">Home</NavLink> |
-      <NavLink to="/ads">Teacher Ads</NavLink> |
+      <img src={logo} alt="logo" width="80px"></img> |
+      <NavLink to="/">Home</NavLink> |<NavLink to="/ads">Teacher Ads</NavLink> |
       <NavLink to="/subjects">Subjects</NavLink> |
-
+      <NavLink to="/subjects/add">Create Subject</NavLink>|
       {isLoggedIn && (
         <>
-        <NavLink to="/ads/create">Create New Ad</NavLink> |
+          <NavLink to="/ads/create">Create New Ad</NavLink> |
           <span>Welcome, {user.email} </span> |
           <button onClick={logOutUser}>Logout</button>
         </>
       )}
-
       {!isLoggedIn && (
         <>
           <NavLink to="/signup">Register</NavLink> |
