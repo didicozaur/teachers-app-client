390444c8911a707fb64977fdba9bd34740d1d253
didicozaur <moisin.diana@yahoo.com>
2022-05-24 09:00:25 +0200

    added bootstap to the NavBar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,30 +4,61 @@ import { AuthContext } from "../context/auth.context";
 import logo from "../images/logo.png";
 
 function Navbar() {
-  const { isLoggedIn, user, logOutUser } = useContext(AuthContext);
+  const { isLoggedIn, logOutUser } = useContext(AuthContext);
 
   return (
-    <nav className="Navbar">
-      <NavLink to="/">
-        <img src={logo} alt="logo" width="80px"></img>
-      </NavLink>
-      <NavLink to="/ads">Teacher Ads</NavLink> |
-      <NavLink to="/subjects">Subjects</NavLink> |
-      {isLoggedIn && (
-        <>
-          <NavLink to="/ads/add">Create New Ad</NavLink> |
-          <NavLink to="/subjects/add">Create Subject</NavLink>|
-          <span>Welcome, {user.email} </span> |
-          <button onClick={logOutUser}>Logout</button>
-        </>
-      )}
-      {!isLoggedIn && (
-        <>
-          <NavLink to="/signup">Register</NavLink> |
-          <NavLink to="/login">Login</NavLink>
-        </>
-      )}
-    </nav>
+    <div className="container">
+      <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-3 mb-4 ">
+        <NavLink
+          className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none"
+          to="/"
+        >
+          <img src={logo} alt="logo" width="80px"></img>
+        </NavLink>
+
+        <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
+          <li>
+            <NavLink to="/ads" className="nav-link px-2 link-dark">
+              Ads
+            </NavLink>
+          </li>
+
+          {isLoggedIn && (
+            <li>
+              <NavLink to="/ads/add">Create New Ad</NavLink>
+            </li>
+          )}
+          <li>
+            <NavLink to="/subjects" className="nav-link px-2 link-dark">
+              Subjects
+            </NavLink>
+          </li>
+        </ul>
+
+        <div className="col-md-3 text-end">
+          {!isLoggedIn && (
+            <>
+              <NavLink className="btn btn-outline-primary me-2" to="/signup">
+                Register
+              </NavLink>
+              <NavLink className="btn btn-primary" to="/login">
+                Login
+              </NavLink>
+            </>
+          )}
+          {isLoggedIn && (
+            <>
+              <NavLink className="btn btn-outline-primary me-2" to="/profile">
+                Profile
+              </NavLink>
+              <button className="btn btn-primary" onClick={logOutUser}>
+                Logout
+              </button>
+            </>
+          )}
+        </div>
+      </header>
+    </div>
   );
 }
 
