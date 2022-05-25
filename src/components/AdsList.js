ab37179eaace7c7da597e039249cf1f0c4355b6f
didicozaur <moisin.diana@yahoo.com>
2022-05-25 08:32:40 +0200

    added styles to the AdList component

--- src/components/AdsList.js
+++ src/components/AdsList.js
@@ -24,41 +24,66 @@ export default function AdsList() {
   }, []);
 
   return (
-    <div>
-      <input
-        type="search"
-        placeholder="Search your Subject"
-        value={subject}
-        onClick={(e) => {
-          filterAds(e);
-        }}
-        onChange={(e) => filterAds(e)}
-      />
-      {subject === ""
-        ? ads.map((ad) => {
-            return (
-              <div key={ad._id}>
-                <p>{ad.title}</p>
-                <p>{ad.location}</p>
-                <p>{ad.level}</p>
-                <NavLink to={`/ads/${ad._id}`}>More details</NavLink> |
-              </div>
-            );
-          })
-        : ads
-            .filter((ad) =>
-              ad.title.toLowerCase().includes(subject.toLowerCase())
-            )
-            .map((ad) => {
+    <>
+      <div className="row g-4 mb-4">
+        <div className="col">
+          <input
+            className="form-control"
+            type="search"
+            placeholder="Search for something new to learn"
+            value={subject}
+            onClick={(e) => {
+              filterAds(e);
+            }}
+            onChange={(e) => filterAds(e)}
+          />
+        </div>
+      </div>
+      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+        {subject === ""
+          ? ads.map((ad) => {
               return (
-                <div key={ad._id}>
-                  <p>{ad.title}</p>
-                  <p>{ad.location}</p>
-                  <p>{ad.level}</p>
-                  <NavLink to={`/ads/${ad._id}`}>More details</NavLink> |
+                <div className="col" key={ad._id}>
+                  <div className="card">
+                    <div className="card-body">
+                      <h5 className="card-title">{ad.title}</h5>
+                      <p className="card-text">Location: {ad.location}</p>
+                      <p className="card-text">Level: {ad.level}</p>
+                      <NavLink
+                        className="btn btn-success"
+                        to={`/ads/${ad._id}`}
+                      >
+                        More details
+                      </NavLink>
+                    </div>
+                  </div>
                 </div>
               );
-            })}
-    </div>
+            })
+          : ads
+              .filter((ad) =>
+                ad.title.toLowerCase().includes(subject.toLowerCase())
+              )
+              .map((ad) => {
+                return (
+                  <div className="col" key={ad._id}>
+                    <div className="card">
+                      <div className="card-body">
+                        <h5 className="card-title">{ad.title}</h5>
+                        <p className="card-text">{ad.location}</p>
+                        <p className="card-text">{ad.level}</p>
+                        <NavLink
+                          className="btn btn-success"
+                          to={`/ads/${ad._id}`}
+                        >
+                          More details
+                        </NavLink>
+                      </div>
+                    </div>
+                  </div>
+                );
+              })}
+      </div>
+    </>
   );
 }
