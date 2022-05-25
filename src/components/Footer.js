3d71d48426ae5f88be166187891ee3e7a8720a53
didicozaur <moisin.diana@yahoo.com>
2022-05-25 07:54:07 +0200

    styled Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,20 @@
-import React from "react"
+import React from "react";
 
-function Footer(){
-    return(
-        <>
-            <p>Made with love by Diana and Mafalda | <a href="mailto:hege@example.com">Contact us</a></p>
-        </>
-    )
+function Footer() {
+  return (
+    <footer className="container text-center mt-4">
+      <div className="row">
+        <div className="col">
+          <p className="small">
+            Made with love by Diana & Mafalda |{" "}
+            <a className="text-success" href="mailto:hege@example.com">
+              Contact us
+            </a>
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
 }
 
-export default Footer
+export default Footer;
