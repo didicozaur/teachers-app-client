084b463f8d493f5494ba4b68ce6103ccf48ba677
mafaldao21 <mafalda.oliveira.21@icloud.com>
2022-05-26 18:36:26 +0100

    add conditional

--- src/components/AdCard.js
+++ src/components/AdCard.js
@@ -5,12 +5,12 @@ import { AuthContext } from "../context/auth.context";
 function AdCard({ ad }) {
   const { isLoggedIn } = useContext(AuthContext);
   return (
-    <div className="col" key={ad._id}>
+    <div className="col" key={ad?._id}>
       <div className="card">
-        <div className="card-header">{ad.subject.title}</div>
+        <div className="card-header">{ad?.subject?.title}</div>
         <div className="card-body">
-          <h5 className="card-title">{ad.title}</h5>
-          <p className="card-text">Location: {ad.location}</p>
+          <h5 className="card-title">{ad?.title}</h5>
+          <p className="card-text">Location: {ad?.location}</p>
           {isLoggedIn && (
             <NavLink className="btn btn-success" to={`/ads/${ad._id}`}>
               More details
