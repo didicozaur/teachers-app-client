1cb2189ecf64657cbbae68477747b8c73ab2568e
didicozaur <moisin.diana@yahoo.com>
2022-05-26 14:35:18 +0200

    used props and deleted unnecessary code

--- src/pages/SubjectsPage.js
+++ src/pages/SubjectsPage.js
@@ -1,28 +1,13 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-function SubjectsPage() {
-  const [subjects, setSubjects] = useState([]);
-
-  const getSubjects = () => {
-    axios
-      .get(`${process.env.REACT_APP_API_URL}/subjects`)
-      .then((response) => setSubjects(response.data))
-      .catch((e) => console.log("Error getting Subjects from API", e));
-  };
-
-  useEffect(() => {
-    getSubjects();
-  }, []);
-
+function SubjectsPage(props) {
   return (
     <div className="container">
       <div className="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4 mb-4">
-        {subjects.map((subject) => {
+        {props.subjects.map((subject) => {
           return (
-            <div className="col">
-              <div className="card" key={subject._id}>
+            <div className="col" key={subject._Id}>
+              <div className="card">
                 <div className="card-body text-center">
                   <Link
                     className="text-success text-decoration-none"
